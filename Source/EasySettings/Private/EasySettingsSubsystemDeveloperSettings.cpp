271ee4b6110cf5ae026b161dc450289dca9d261d
Wellsaik <artem.podorozhko@outlook.com>
2024-08-19 23:54:26 +0300

    Easy settings: developer settings

---
+++ Source/EasySettings/Private/EasySettingsSubsystemDeveloperSettings.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EasySettingsSubsystemDeveloperSettings.h"
+
+UEasySettingsSubsystemDeveloperSettings::UEasySettingsSubsystemDeveloperSettings(
+	const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
---
+++ Source/EasySettings/Public/EasySettingsSubsystemDeveloperSettings.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "EasySettingsSubsystemDeveloperSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config=Editor, defaultconfig, meta = (DisplayName="Easy Settings"))
+class EASYSETTINGS_API UEasySettingsSubsystemDeveloperSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	UEasySettingsSubsystemDeveloperSettings(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+};
